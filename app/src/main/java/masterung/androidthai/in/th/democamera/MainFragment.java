665d3dd9562452ce9b332cf9296b0a754ca7de1d
masterUNG <phrombutr@gmail.com>
2018-09-06 05:34:21 +0700

    Update 6Sep

--- app/src/main/java/masterung/androidthai/in/th/democamera/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/democamera/MainFragment.java
@@ -6,12 +6,18 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.util.Random;
 
 
 /**
@@ -36,6 +42,36 @@ public class MainFragment extends Fragment {
 //        Gallry Controller
         gallryController();
 
+//        Camera Controller
+        cameraController();
+
+    }
+
+    private void cameraController() {
+        ImageView imageView = getView().findViewById(R.id.imvCamera);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String pathFileString = Environment.getExternalStorageDirectory() + "/" + "MasterUNG";
+                Log.d("6SepV1", "pathFileString ==> " + pathFileString);
+
+                File file = new File(pathFileString);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+
+                Random random = new Random();
+                int i = random.nextInt(1000);
+                File cameraFile1 = new File(file, "master_" + Integer.toString(i) + ".jpg");
+
+                uri = Uri.fromFile(cameraFile1);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(intent, 2);
+
+            }   // onClick
+        });
     }
 
     @Override
@@ -47,24 +83,31 @@ public class MainFragment extends Fragment {
                 case 1:
                     uri = data.getData();
 
-                    try {
+                    showImage();
 
-                        Bitmap bitmap = BitmapFactory
-                                .decodeStream(getActivity()
-                                        .getContentResolver().openInputStream(uri));
-                        Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,
-                                800, 600, false);
-                        photoImageView.setImageBitmap(bitmap1);
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
+                    break;
+                case 2:
+                    showImage();
                     break;
             }
         }   // if
 
     }   // onActivityResult
+
+    private void showImage() {
+        try {
+
+            Bitmap bitmap = BitmapFactory
+                    .decodeStream(getActivity()
+                            .getContentResolver().openInputStream(uri));
+            Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,
+                    800, 600, false);
+            photoImageView.setImageBitmap(bitmap1);
+
+        } catch (Exception e) {
+            Log.d("6SepV1", "e showImage ==> " + e.toString());
+        }
+    }
 
     private void gallryController() {
         ImageView imageView = getView().findViewById(R.id.imvGallry);
